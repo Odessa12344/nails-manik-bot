7c489973482a5b65b173e143280534609a496203
Odessa12344 <119266353+Odessa12344@users.noreply.github.com>
2026-02-26 06:00:06 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8676532226:AAGwKfNaDE_edfoJuekVSS76VkMw50_zIUY")
+ADMIN_ID = int(os.getenv("@nuntrs"))
+CHANNEL_ID = int(os.getenv("-100..."))
+CHANNEL_LINK = os.getenv("https://t.me/Nails001_Manik")
